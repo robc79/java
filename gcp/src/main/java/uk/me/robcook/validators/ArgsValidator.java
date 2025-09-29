5fa583ef3582ae230e63aec2329f984463c3e2f5
Rob Cook <message@robcook.me.uk>
2025-09-29 16:38:34 +0100

    simple args validator for gcp.

--- gcp/src/main/java/uk/me/robcook/validators/ArgsValidator.java
+++ gcp/src/main/java/uk/me/robcook/validators/ArgsValidator.java
@@ -1,12 +1,29 @@
 package uk.me.robcook.validators;
 
+import java.io.File;
+
 public class ArgsValidator implements ValidateArgs
 {
     @Override
     public boolean Validate(String[] args)
     {
-        // Validate the supplied args.
-        
+        if (args.length != 1)
+        {
+            return false;
+        }
+
+        var file = new File(args[0]);
+
+        if (!file.exists())
+        {
+            return false;
+        }
+
+        if (!file.getName().endsWith(".col"))
+        {
+            return false;
+        }
+
         return true;
     }
 }
