563a2399c87d23a679e24a2426450308b8c84523
Rob Cook <message@robcook.me.uk>
2025-11-06 19:11:32 +0000

    Tidied up command help.

--- rosalind/src/main/java/uk/me/robcook/rosalind/handlers/HelpHandler.java
+++ rosalind/src/main/java/uk/me/robcook/rosalind/handlers/HelpHandler.java
@@ -3,6 +3,7 @@ package uk.me.robcook.rosalind.handlers;
 import java.io.PrintStream;
 import java.util.Arrays;
 
+import uk.me.robcook.rosalind.commands.Command;
 import uk.me.robcook.rosalind.commands.CommandName;
 import uk.me.robcook.rosalind.commands.HelpCommand;
 import uk.me.robcook.rosalind.commands.ParseCommand;
@@ -23,30 +24,40 @@ public class HelpHandler implements CommandHandler<HelpCommand>
     {
         if (command.getArgs().length == 0)
         {
-            out.println("Usage: Ros command args");
-            out.println("");
-            out.print("Where command is one of: ");
-
-            for(var commandName : CommandName.values())
-            {
-                out.print(String.format("%s, ", commandName));
-            }
-
-            out.println();
-            out.println("For help on a specific command: Ros help command");
+            dumpGenericHelp();
         }
         else
         {
-            var commandArgs = command.getArgs();
-            var commandName = CommandName.valueOf(commandArgs[0]);
-
-            var targetCommand = switch (commandName)
-            {
-                case CommandName.parse -> new ParseCommand(commandArgs);
-                default -> new HelpCommand(commandArgs);
-            };
-
-            out.println(targetCommand.getHelpText());
+            dumpCommandHelp(command);
         }
+    }
+
+    private void dumpGenericHelp()
+    {
+        out.println("Usage: Ros command args");
+        out.println("");
+        out.print("Where command is one of: ");
+
+        for(var commandName : CommandName.values())
+        {
+            out.print(String.format("%s, ", commandName));
+        }
+
+        out.println();
+        out.println("For help on a specific command: Ros help command");
+    }
+
+    private void dumpCommandHelp(Command command)
+    {
+        var commandArgs = command.getArgs();
+        var commandName = CommandName.valueOf(commandArgs[0]);
+
+        var targetCommand = switch (commandName)
+        {
+            case CommandName.parse -> new ParseCommand(commandArgs);
+            default -> new HelpCommand(commandArgs);
+        };
+
+        out.println(targetCommand.getHelpText());
     }
 }
